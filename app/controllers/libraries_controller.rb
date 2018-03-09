6c4703d239c4641bb2eadbcc0a1e26ccc07dbb89
Patryk Stępień <droznyk@gmail.com>
2018-03-09 21:02:10 +0100

    Add show and destroy action

--- app/controllers/libraries_controller.rb
+++ app/controllers/libraries_controller.rb
@@ -1,4 +1,5 @@
 class LibrariesController < ApplicationController
+  before_action :fetch_library, only: %w[show edit destroy]
 
   def index
     @libraries = Library.all
@@ -11,18 +12,30 @@ class LibrariesController < ApplicationController
   def create
     @library = Library.new(library_params)
     if @library.save
-      redirect_to libraries_path, 
-        notice: "Library has been created successfully!", method: :get
+      redirect_to library_path(@library), 
+        notice: "Library has been created successfully!"
     else
       flash[:error] = @library.errors.full_messages
       render :new
     end
   end
 
+  def show; end
+
+  def destroy
+    flash[:notice] = "#{@library.name.capitalize} has been destroyed successfully!"
+    @library.destroy
+    redirect_to libraries_path
+  end
+
   private
 
   def library_params
     params.require(:library).permit(:name, :description)
+  end
+
+  def fetch_library
+    @library = Library.find(params[:id])
   end
 
 end
