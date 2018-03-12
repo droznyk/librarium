5f98a90c6e8f0d114717902edd3bfaafb782b2c5
Patryk Stępień <droznyk@gmail.com>
2018-03-12 20:00:13 +0100

    Create books table

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180311125154) do
+ActiveRecord::Schema.define(version: 20180312184137) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "books", force: :cascade do |t|
+    t.string "author"
+    t.string "title"
+    t.string "short_description"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "libraries", force: :cascade do |t|
     t.string "name"
