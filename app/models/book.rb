958f7c4db1b75d6bb37f642d08b4e10681db93aa
Patryk Stępień <droznyk@gmail.com>
2018-03-20 13:14:13 +0100

    Fix minor errors like typo

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,5 +1,5 @@
 class Book < ApplicationRecord
-  validates :author, presence: :true, length: { in 3..80}
+  validates :author, presence: true, length: { in: 3..80}
   validates :title, presence: true, uniqueness: true, length: { in: 3..150}
   validates :short_description, length: { maximum: 300}
 end
